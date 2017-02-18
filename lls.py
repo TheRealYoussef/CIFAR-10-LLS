5932cb0ca317a63781ed7fe72945dd7d62152099
Youssef Khairalla <therealyoussef@aucegypt.edu>
2017-02-18 09:43:38 +0200

    Update lls.py

--- lls.py
+++ lls.py
@@ -7,17 +7,25 @@ class LLS(object):
         pass
 
     def train(self, data, labels):
-        x = np.ones((data.shape[0], data.shape[1] + 1), dtype=np.float32)
-        x[:, :data.shape[1]] = data
+        # data is an n*d np array (n is the number of data points, d is the number of features)
+        # labels is an np array of size n containing the corresponding labels (integers from 0 (inclusive) to c (exclusive))
+        # where c is the number of classes
+        n = data.shape[0]
+        d = data.shape[1]
+        c = len(set(labels))
+        x = np.ones((n, d + 1), dtype=np.float32)
+        x[:, :d] = data
         inverse = inv(np.dot(x.T, x))
-        self.w = np.empty((10, data.shape[1] + 1))
-        for i in range(10):
+        self.w = np.empty((c, d + 1))
+        for i in range(c):
             t = np.array([1.0 if i == y else 0.0 for y in labels])[:, np.newaxis]
             self.w[i] = np.dot(inverse, np.dot(x.T, t))[:,0]
 
     def predict(self, data):
-        y_pred = np.empty(data.shape[0], dtype=np.uint8)
-        x = np.append(data.T, np.ones((1, data.shape[0]), dtype=np.float32), axis=0)
+        # data is an n*d np array (n is the number of data points, d is the number of features)
+        n = data.shape[0]
+        y_pred = np.empty(n, dtype=np.uint8)
+        x = np.append(data.T, np.ones((1, n), dtype=np.float32), axis=0)
         f = np.dot(self.w, x).T
         for i in range(f.shape[0]):
             scores = f[i]
